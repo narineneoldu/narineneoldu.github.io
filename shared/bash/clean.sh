e85383c208affe9aceb9c517be09b8003b900409
Ismail SEZEN <narineneoldu@gmail.com>
2025-11-25 05:11:24 +0300

    Update clean.sh

--- shared/bash/clean.sh
+++ shared/bash/clean.sh
@@ -3,4 +3,4 @@
 # Hem en hem tr temp tsv dosyalarını siler
 find . -type f \( -name ".qrender-time.tmp-en.tsv" -o -name ".qrender-time.tmp-tr.tsv" \) -delete
 find . -type f \( -name "reading-time-debug.log" -o -name "reading-time-debug.log" \) -delete
-find . -type f -name '*_reading_stats.yml' -delete
+# find . -type f -name '*_reading_stats.yml' -delete
