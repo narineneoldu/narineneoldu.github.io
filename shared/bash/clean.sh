f15b699f7fbbdf2d51727538d6393c8ffd1be47c
Ismail SEZEN <narineneoldu@gmail.com>
2025-11-29 21:21:33 +0300

    Update clean.sh

--- shared/bash/clean.sh
+++ shared/bash/clean.sh
@@ -4,3 +4,4 @@
 find . -type f \( -name ".qrender-time.tmp-en.tsv" -o -name ".qrender-time.tmp-tr.tsv" \) -delete
 find . -type f \( -name "reading-time-debug.log" -o -name "reading-time-debug.log" \) -delete
 # find . -type f -name '*_reading_stats.yml' -delete
+# find . -type f -name '*gram.txt' -delete
